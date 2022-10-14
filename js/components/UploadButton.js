eec361884d02dd8fd4d5d38a5893640ec0ad02f8
Luke Wu <lukewu92@gmail.com>
2022-10-14 19:33:07 +0800

    support adding more files

--- js/components/UploadButton.js
+++ js/components/UploadButton.js
@@ -21,32 +21,47 @@ export class UploadButton {
     this.fileInput.addEventListener(
       "change",
       () => {
+        const oldFiles = [...(this.getImageData()?.files || [])];
         const files = this.fileInput.files;
-        const fileInfos = [];
+        const fileInfos = oldFiles;
+        const offsetIndex = oldFiles.length;
+        let filesAdded = 0;
+
         for (let i = 0; i < files.length; i++) {
+          const alreadyExist = Boolean(
+            oldFiles.find((f) => f.name === files[i].name)
+          );
+          if (alreadyExist) continue;
+          filesAdded++;
+
           const img = new Image();
           img.onload = () => {
             /* Uncomment to store image data to localStorage, limited to 5MB per browser */
             const base64Data = getBase64Image(img);
             // add width and height to properties after loaded
             const newFileList = [...this.getImageData().files];
+            let fileIndex = newFileList.findIndex((f) => f.name === img.name);
+
+            fileIndex = fileIndex < 0 ? i + offsetIndex : fileIndex;
 
             const newProperties = {
-              ...newFileList[i],
+              ...newFileList[fileIndex],
               width: img.width,
               height: img.height,
               base64Data: base64Data,
             };
-            newFileList[i] = newProperties;
+
+            newFileList[fileIndex] = newProperties;
+            console.log("newFileList", newFileList);
+
             this.setImageData({
               files: newFileList,
             });
           };
           img.src = URL.createObjectURL(files[i]);
-
           // Save image properties to local data
           const file = files[i];
-          const fileName = file.name;
+          // Check if file already exist in list
           fileInfos.push({
             name: file.name,
             type: file.type,
@@ -57,13 +72,18 @@ export class UploadButton {
           });
         }
 
-        this.setImageData({
-          files: fileInfos,
-        });
-        this.setState({
-          ...this.getState(),
-          selectedFileIndex: 0,
-        });
+        if (filesAdded > 0) {
+          this.setImageData({
+            files: fileInfos,
+          });
+
+          this.setState({
+            ...this.getState(),
+            selectedFileIndex: offsetIndex + files.length - 1,
+          });
+        } else {
+          alert("File already exist!");
+        }
       },
       false
     );
